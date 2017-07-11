2ce7de47fa75de6eed94ea43a8e0c0c465ab4dfd
khomiakAndriy <khomiak.andriy@gmail.com>
2017-07-11 22:55:27 +0300

    11-07(1)

--- src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,14 +1,37 @@
 package com.luv2code.springdemo.mvc;
 
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 public class Customer {
     private String firstName;
     @NotNull (message = "is required")
-    @Size (min = 1)
+    @Size (min = 1, message = "is required")
     private String lastName;
+
+    @NotNull (message = "is required")
+    @Min(value = 0, message = "must be greater tnen or equel to 0 ")
+    @Max(value = 10, message = "must be less tnen or equel to 10 ")
+    private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
 
     public String getFirstName() {
         return firstName;
--- src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
+++ src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
@@ -1,19 +1,49 @@
 package com.luv2code.springdemo.mvc;
 
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+    // add an initbinder ... to convert trim input strings
+    // remove leading and trailing whitespace
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @RequestMapping("/showForm")
     public String showForm(Model theModel){
 
         theModel.addAttribute("customer", new Customer());
         return "customer-form";
+    }
+
+    // @valid@ is one for all type of validation errors
+
+    @RequestMapping("/processForm")
+    public String processForm(
+            @Valid @ModelAttribute("customer") Customer theCustomer, BindingResult bindingResult){
+        System.out.println("Last name: |" + theCustomer.getLastName()+"|");
+
+        if (bindingResult.hasErrors()){
+            return "customer-form";
+        } else {
+            return "customer-confitmation";
+        }
     }
 
 }
